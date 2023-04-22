e93fcff6001a04b0c96c93936e8589cc3b2e2340
Brodie Ashcraft <brodiea19@gmail.com>
2023-04-22 10:52:59 -0700

    sending data is so fetch

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, eMail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: eMail,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const identity = data.id;
+      document.querySelector("body").append(identity);
+    })
+    .catch(() => document.querySelector("body").append("Unauthorized Access"));
+}
+
+submitData("joe", "joemail");
